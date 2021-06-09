35a6a22f4069381de7998779d9a6246089e529ce
Loakesh I <loakesh.indiran@wowlabz.com>
2021-06-09 19:06:46 +0530

    Add: Code to send Json responses Implemented:
    Because:

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::serde::json::json;
+use rocket::serde::json::Value;
+
 #[get("/")]
-fn hello() -> &'static str {
-    "Hello, world!"
+fn hello() -> Value {
+    json!({"success": true, "data": "Hello, world!"})
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![hello])
 }
-
