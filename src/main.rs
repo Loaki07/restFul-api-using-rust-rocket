990959bc702f5f9f92161c4e3304289d3af5c170
Loakesh I <loakesh.indiran@wowlabz.com>
2021-06-09 20:04:01 +0530

    Add: functions for get, post, put & delete methods Implemented:
    Because:

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::response::status;
 use rocket::serde::json::json;
 use rocket::serde::json::Value;
 
@@ -9,7 +10,67 @@ fn hello() -> Value {
     json!({"success": true, "data": "Hello, world!"})
 }
 
+#[get("/rustaceans")]
+fn get_rustacean() -> Value {
+    json!([
+        {
+            "id": 1,
+            "name": "John Doe"
+        },
+        {
+            "id": 2,
+            "name": "Adam"
+        },
+        {
+            "id": 3,
+            "name": "Eve"
+        },
+    ])
+}
+
+#[get("/rustaceans/<id>")]
+fn view_rustacean(id: i32) -> Value {
+    json!({
+        "id": id,
+        "name": "John Doe",
+        "email": "john@doe.com"
+    })
+}
+
+#[post("/rustaceans", format = "json")]
+fn create_rustacean() -> Value {
+    json!({
+        "id": 1,
+        "name": "John Doe",
+        "email": "john@doe.com"
+    })
+}
+
+#[put("/rustaceans/<id>", format = "json")]
+fn update_rustacean(id: i32) -> Value {
+    json!({
+        "id": id,
+        "name": "John Doe",
+        "email": "john@doe.com"
+    })
+}
+
+#[delete("/rustaceans/<_id>")]
+fn delete_rustacean(_id: i32) -> status::NoContent {
+    status::NoContent
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello])
+    rocket::build().mount(
+        "/",
+        routes![
+            hello,
+            get_rustacean,
+            create_rustacean,
+            view_rustacean,
+            update_rustacean,
+            delete_rustacean
+        ],
+    )
 }
