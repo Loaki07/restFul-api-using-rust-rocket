cc2bac58b2a4e941487df98fad011dd4bfe5ed04
Loakesh I <loakesh.indiran@wowlabz.com>
2021-06-09 22:15:44 +0530

    Add: Auth with username and password for routes thereby protecting the routes Implemented:
    Because:

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::http::Status;
+use rocket::request::{FromRequest, Outcome, Request};
 use rocket::response::status;
 use rocket::serde::json::json;
 use rocket::serde::json::Value;
@@ -11,7 +13,8 @@ fn hello() -> Value {
 }
 
 #[get("/rustaceans")]
-fn get_rustacean() -> Value {
+fn get_rustacean(auth: BasicAuth) -> Value {
+    println!("{:#?}", auth);
     json!([
         {
             "id": 1,
@@ -29,7 +32,7 @@ fn get_rustacean() -> Value {
 }
 
 #[get("/rustaceans/<id>")]
-fn view_rustacean(id: i32) -> Value {
+fn view_rustacean(id: i32, _auth: BasicAuth) -> Value {
     json!({
         "id": id,
         "name": "John Doe",
@@ -38,7 +41,7 @@ fn view_rustacean(id: i32) -> Value {
 }
 
 #[post("/rustaceans", format = "json")]
-fn create_rustacean() -> Value {
+fn create_rustacean(_auth: BasicAuth) -> Value {
     json!({
         "id": 1,
         "name": "John Doe",
@@ -47,7 +50,7 @@ fn create_rustacean() -> Value {
 }
 
 #[put("/rustaceans/<id>", format = "json")]
-fn update_rustacean(id: i32) -> Value {
+fn update_rustacean(id: i32, _auth: BasicAuth) -> Value {
     json!({
         "id": id,
         "name": "John Doe",
@@ -56,7 +59,7 @@ fn update_rustacean(id: i32) -> Value {
 }
 
 #[delete("/rustaceans/<_id>")]
-fn delete_rustacean(_id: i32) -> status::NoContent {
+fn delete_rustacean(_id: i32, _auth: BasicAuth) -> status::NoContent {
     status::NoContent
 }
 
@@ -65,6 +68,62 @@ fn not_found() -> Value {
     json!({ "success": false, "data": "Not found!"})
 }
 
+#[derive(Debug)]
+pub struct BasicAuth {
+    pub username: String,
+    pub password: String,
+}
+
+impl BasicAuth {
+    fn from_authorization_header(header: &str) -> Option<BasicAuth> {
+        let split = header.split_whitespace().collect::<Vec<_>>();
+
+        if split.len() != 2 {
+            return None;
+        }
+
+        if split[0] != "Basic" {
+            return None;
+        }
+
+        Self::from_base64_encoded(split[1])
+    }
+
+    fn from_base64_encoded(base64_string: &str) -> Option<BasicAuth> {
+        let decoded = base64::decode(base64_string).ok()?;
+        let decoded_str = String::from_utf8(decoded).ok()?;
+        let split = decoded_str.split(":").collect::<Vec<_>>();
+
+        // If exactly username & password pair are present
+        if split.len() != 2 {
+            return None;
+        }
+
+        let (username, password) = (split[0].to_string(), split[1].to_string());
+
+        Some(BasicAuth { username, password })
+    }
+}
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for BasicAuth {
+    type Error = ();
+
+    async fn from_request(request: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        let auth_header = request.headers().get_one("Authorization");
+        if let Some(auth_header) = auth_header {
+            if let Some(auth) = Self::from_authorization_header(auth_header) {
+                return Outcome::Success(auth);
+            }
+        }
+
+        Outcome::Failure((Status::Unauthorized, ()))
+    }
+}
+
+/**
+ * Authorization Header: Authorization: Basic QWxhZGRpbjpPcGVuU2VzYW1l
+ */
 #[launch]
 fn rocket() -> _ {
     rocket::build()
